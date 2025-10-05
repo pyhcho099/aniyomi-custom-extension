e2a75e26d39e4ac0da42676cb4344f8b049f3472
pyhcho099 <155559163+pyhcho099@users.noreply.github.com>
2025-10-05 18:34:47 +0530

    Update build.gradle.kts

--- src/en/anigo/build.gradle.kts
+++ src/en/anigo/build.gradle.kts
@@ -1,7 +1,12 @@
+apply(plugin = "com.android.application")
+apply(plugin = "kotlin-android")
+
 ext {
     extName = "Anigo"
+    pkgNameSuffix = "en.anigo"
     extClass = ".Anigo"
     extVersionCode = 1
+    libVersion = "14"
 }
 
 apply(from = "$rootDir/common.gradle")
